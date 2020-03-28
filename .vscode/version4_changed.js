0b56718e0aef8507415267cc9f36d80f5006faeb
ShtankoDaria <daria.shtanko@gmail.com>
2020-03-28 13:13:13 +0100

    changes

--- .vscode/version4_changed.js
+++ .vscode/version4_changed.js
@@ -1,48 +1,39 @@
 window.onload = () => {
-
-  // var todoList = {
-    
-  //   addTodo: function(todoText) {
-  //     this.todos.push({
-  //       todoText: todoText,
-  //       completed: false
-  //     });
-  //   },
-  //   changeTodo: function(position, todoText) {
-  //     this.todos[position].todoText = todoText;
-  //   },
-  //   toggleCompleted: function(position) {
-  //     var todo = this.todos[position];
-  //     todo.completed = !todo.completed;
-  //   },
   
-  // };
- var handlers = {
-  todos: [],
+  var todoList = {
+    todos: [],
   displayTodos: function() {
     console.log('My todos:', this.todos);
   },
+    addTodo: function(todoText) {
+      this.todos.push({
+        todoText: todoText,
+        completed: false
+      });
+    },
+    changeTodo: function(position, todoText) {
+      this.todos[position].todoText = todoText;
+    },
+    toggleCompleted: function(position) {
+      var todo = this.todos[position];
+      todo.completed = !todo.completed;
+    },
+  
+  };
+ var handlers = {
+  
   addTodo: function () {
       debugger;
       var addTodoTextInput = document.getElementById('addTodoTextInput');
-      handlers.addTodo(addTodoTextInput.value);
+      todoList.addTodo(addTodoTextInput.value);
       addTodoTextInput.value = '';
       this.displayTodos();
   },
   changeTodo: function () {
       debugger;
-      var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
       var changeTodoTextInput = document.getElementById('changeTodoTextInput');
-      todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
-      changeTodoPositionInput.value = '';
+      todoList.changeTodo( changeTodoTextInput.value);
       changeTodoTextInput.value = '';
-      this.displayTodos();
-  },
-  toggleCompleted: function () {
-      debugger;
-      var toggleCompletedPositionInput = document.getElementById('toggleCompletedPositionInput');
-      todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
-      toggleCompletedPositionInput.value = '';
       this.displayTodos();
   },
 }
